d0eef226b1ca625608ed07f64e103c0391d2c1b0
douyongduo <yongduo.dou@cy-tech.net>
2020-07-17 15:49:51 +0800

    提交人：pgli 合入问题：代理及登录 合入时间：2020-7-17

--- App.vue
+++ App.vue
@@ -45,7 +45,8 @@
     /* #ifndef APP-PLUS-NVUE */
     /* uni.css - 通用组件、模板样式库，可以当作一套ui库应用 */
     @import './common/uni.css';
-
+	/* 头条小程序需要把 iconfont 样式放到组件外 */
+	@import "components/m-icon/m-icon.css";
     /* 以下样式用于 hello uni-app 演示所需 */
     page {
         background-color: #F4F5F6;
@@ -82,4 +83,115 @@
     }
 
     /* #endif*/
+	
+
+		
+	/* #ifdef MP-BAIDU */
+	page {
+		width: 100%;
+		height: 100%;
+		display: block;
+	}
+		
+	swan-template {
+		width: 100%;
+		min-height: 100%;
+		display: flex;
+	}
+		
+	/* 原生组件模式下需要注意组件外部样式 */
+	custom-component {
+		width: 100%;
+		min-height: 100%;
+		display: flex;
+	}
+		
+	/* #endif */
+		
+	/* #ifdef MP-ALIPAY */
+	page {
+		min-height: 100vh;
+	}
+		
+	/* #endif */
+		
+	/* 原生组件模式下需要注意组件外部样式 */
+	m-input {
+		width: 100%;
+		/* min-height: 100%; */
+		display: flex;
+		flex: 1;
+	}
+		
+	.content {
+		display: flex;
+		flex: 1;
+		flex-direction: column;
+		background-color: #efeff4;
+		padding: 10px;
+	}
+		
+	.input-group {
+		background-color: #ffffff;
+		margin-top: 20px;
+		position: relative;
+	}
+		
+	.input-group::before {
+		position: absolute;
+		right: 0;
+		top: 0;
+		left: 0;
+		height: 1px;
+		content: '';
+		-webkit-transform: scaleY(.5);
+		transform: scaleY(.5);
+		background-color: #c8c7cc;
+	}
+		
+	.input-group::after {
+		position: absolute;
+		right: 0;
+		bottom: 0;
+		left: 0;
+		height: 1px;
+		content: '';
+		-webkit-transform: scaleY(.5);
+		transform: scaleY(.5);
+		background-color: #c8c7cc;
+	}
+		
+	.input-row {
+		display: flex;
+		flex-direction: row;
+		position: relative;
+		font-size: 18px;
+		line-height: 40px;
+	}
+		
+	.input-row .title {
+		width: 120px;
+		padding-left: 15px;
+	}
+		
+	.input-row.border::after {
+		position: absolute;
+		right: 0;
+		bottom: 0;
+		left: 8px;
+		height: 1px;
+		content: '';
+		-webkit-transform: scaleY(.5);
+		transform: scaleY(.5);
+		background-color: #c8c7cc;
+	}
+		
+	.btn-row {
+		margin-top: 25px;
+		padding: 10px;
+	}
+		
+	button.primary {
+		background-color: #0faeff;
+	}
 </style>
